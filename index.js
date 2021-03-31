9f880fd9c31af8f184d7a9611b367e3a3ced1bf5
daulen <daulenz@gmail.com>
2021-03-31 10:49:23 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element=document.querySelector('body');
+element.children[0].remove();
+
+const newHeader = document.createElement("h1");
+const text=document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(text)
+newHeader.id = "victory";
+const element1=document.getElementById("victory")
+element1.appendChild('body')
